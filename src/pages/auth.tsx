303a9afa968e1d900ae99b5ccea1ab6c3062b81f
Andrew Dobson <tandrewlopez@gmail.com>
2023-03-17 23:36:41 -0400

    created data models

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -10,19 +10,19 @@ const Auth = () => {
 
   const toggleVariant = useCallback(() => {
     setVariant((currentVariant) =>
-      currentVariant === "login" ? "register" : "login"
+      currentVariant === "register" ? "login" : "register"
     );
   }, []);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-center bg-cover">
-      <div className="bg-black w-full h-full lg:bg-opacity-50">
+      <div className="w-full h-full bg-black lg:bg-opacity-50">
         <nav className="px-12 py-5">
           <img src="/images/logo.png" alt="logo" className="h-12" />
         </nav>
         <div className="flex justify-center">
-          <div className="bg-black/70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">
+          <div className="self-center w-full px-16 py-16 mt-2 rounded-md bg-black/70 lg:w-2/5 lg:max-w-md">
+            <h2 className="mb-8 text-4xl font-semibold text-white">
               {variant === "login" ? "Sign in" : "Register"}
             </h2>
             <div className="flex flex-col gap-4">
@@ -57,15 +57,18 @@ const Auth = () => {
                 value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-              {variant === "register" ? "Sign up" : "Log in"}
+            <button className="w-full py-3 mt-10 text-white transition bg-red-600 rounded-md hover:bg-red-700">
+              {variant === "register" ? "Sign up" : "Login"}
             </button>
-            <p className="text-neutral-500 mt-12">
-              First time using Netflix?
+            <p className="mt-12 text-neutral-500">
+              {variant === "register"
+                ? "Already have an account?"
+                : "First time using Netflix?"}
+
               <span
                 onClick={toggleVariant}
-                className="text-white ml-1 hover:underline cursor-pointer">
-                Create an account
+                className="ml-1 text-white cursor-pointer hover:underline">
+                {variant === "register" ? "Login" : " Create an account"}
               </span>
             </p>
           </div>
