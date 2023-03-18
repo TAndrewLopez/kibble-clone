3e237192da28a13cb0978be1f3b4d08e20f8e859
Andrew Dobson <tandrewlopez@gmail.com>
2023-03-18 18:51:04 -0400

    created current user hook and redirect users who aren't logged in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,36 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
 const HomePage = () => {
-  return <h1 className="text-2xl text-green-500"></h1>;
+  const { data: user } = useCurrentUser();
+
+  return (
+    <>
+      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
+      <p className="text-white">Logged in as: {user?.name}</p>
+      <button className="w-full h-10 bg-white" onClick={() => signOut()}>
+        Logout
+      </button>
+    </>
+  );
 };
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export default HomePage;
